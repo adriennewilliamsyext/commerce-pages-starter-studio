bbec760500f4770e307ee30fa369fff71730f658
Adrienne Williams <82108228+adriennewilliamsyext@users.noreply.github.com>
2023-08-03 11:00:18 -0400

    Update product.tsx, remove old RTF

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -28,7 +28,6 @@ export const config: TemplateConfig = {
       "price",
       "primaryPhoto",
       "photoGallery",
-      "richTextDescription",
       "commerce_productDescription",
     ],
   },
